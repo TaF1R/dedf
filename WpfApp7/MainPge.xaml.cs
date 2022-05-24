7b08753ab9c065797d5c063413df8a9809543a06
TaF1R <106156054+TaF1R@users.noreply.github.com>
2022-05-24 18:46:35 +0700

    Добавьте файлы проекта.

---
+++ WpfApp7/MainPge.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp7
+{
+    /// <summary>
+    /// Логика взаимодействия для MainPge.xaml
+    /// </summary>
+    public partial class MainPge : Page
+    {
+        public MainPge()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+           // gost page = new gost();
+            NavigationService.Navigate(new gost());
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Create window = new Create();
+            window.Show();
+            gost page = new gost();
+            NavigationService.Navigate(page);
+        }
+    }
+}
